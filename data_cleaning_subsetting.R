01621da04306732c369e51200be0810bd3f64e5a
sisitzky <90338095+sisitzky@users.noreply.github.com>
2021-10-16 20:14:41 -0400

    Data Cleaning: Subsetting

---
+++ data_cleaning_subsetting.R
@@ -0,0 +1,29 @@
+## data cleaning, subsetting
+
+source("~/Desktop/MSSP Consulting/G2-Consulting-Project/facial_zone_dummy.R")
+
+rownames(mmc_data)
+
+mmc_data <- mmc_data[-c(319),]
+
+mmc_data$Age.18 <- ifelse(mmc_data$Age.18 == "*", 1, 0)
+mmc_data$Age.89 <- ifelse(mmc_data$Age.89 == "*", 1, 0)
+
+mmc_data[mmc_data=="                       "] <- NA
+mmc_data[mmc_data=="                        "] <- NA
+
+data.frame(colnames(mmc_data))
+
+mmc_subset <-  mmc_data[c(264:266, 1:10, 48:80, 262)]
+
+data.frame(colnames(mmc_subset))
+
+
+
+
+mmc_subset <- mmc_subset[complete.cases(mmc_subset$ICD10.diagnosis.codes.1),]
+
+## ____________________
+
+mmc_subset_ICD9_ICD10 <- mmc_data[c(264:266, 1:10, 22:80, 262)]
+
