6cfa8607486b7bc34f348ece6a9a7c55ce956cb6
sisitzky <90338095+sisitzky@users.noreply.github.com>
2021-10-18 15:24:44 -0400

    Data Cleaning / Subsetting

--- data_cleaning_subsetting.R
+++ data_cleaning_subsetting.R
@@ -33,7 +33,7 @@ mmc_subset_ICD9 <- mmc_data[c(1:9, 264:267, 22:47, 262)]
 mmc_subset_ICD9 <- mmc_subset_ICD9[complete.cases(mmc_subset_ICD9_ICD10$ICD9.diagnosis.codes.1),]
 
 ## Creating test data frame to observe mechanisms vs category
-test <- data.frame(c(mmc_data[c(10)], mmc_subset_ICD9_ICD10[c(13)]))
+test <- mmc_data[c(10, 267)]
 
 
 
