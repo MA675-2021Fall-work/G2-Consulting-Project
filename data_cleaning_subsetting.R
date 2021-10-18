62ff7c9e8560048e9a3a0b965b5e2e1e6f20705d
newbieDX <chenxubu@bu.edu>
2021-10-17 23:21:56 -0400

    Adding the new injury.Mech.category column to subsets

--- data_cleaning_subsetting.R
+++ data_cleaning_subsetting.R
@@ -1,7 +1,7 @@
 ## data cleaning, subsetting
 
 ## Sourcing dummy variables file
-source("~/Desktop/MSSP Consulting/G2-Consulting-Project/facial_zone_dummy.R")
+source("facial_zone_dummy.R")
 
 ## removing blank row from mmc_data
 mmc_data <- mmc_data[-c(319),]
@@ -16,16 +16,19 @@ mmc_data[mmc_data=="                        "] <- NA
 
 # ____________________
 
-## creating first subset, only contains ICD10 code columns
-mmc_subset_ICD10 <-  mmc_data[c(264:266, 1:10, 48:80, 262)]
+## creating first subset, only contains ICD10 code columns, with our Categorized column J
+mmc_subset_ICD10 <-  mmc_data[c(1:9, 264:267, 48:80, 262)]
 
 ## removing rows that do not have a value in the ICD10.diagnosis.code.1 variable
-mmc_subset_ICD10 <- mmc_subset_ICD10[complete.cases(mmc_subset_ICD10$ICD10.diagnosis.codes.1),]
+mmc_subset_ICD10_clean <- mmc_subset_ICD10[complete.cases(mmc_subset_ICD10$ICD10.diagnosis.codes.1),]
 
 # ____________________
 
 ## creating another subset which includes both the ICD9 and ICD10 code columns
-mmc_subset_ICD9_ICD10 <- mmc_data[c(264:266, 1:10, 22:80, 262)]
+mmc_subset_ICD9_ICD10 <- mmc_data[c(1:9, 264:267, 22:80, 262)]
+
+## removing rows that do not have a value in ICD.9.diagnosis.code.1 variable
+mmc_subset_ICD9_ICD10_conv <- mmc_subset_ICD9_ICD10[complete.cases(mmc_subset_ICD9_ICD10$ICD9.diagnosis.codes.1),]
 
 
 
