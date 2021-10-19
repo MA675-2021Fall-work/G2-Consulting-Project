b5846e0f2131efc74af8454a88b5132388b4d07f
sisitzky <90338095+sisitzky@users.noreply.github.com>
2021-10-19 16:22:14 -0400

    Updated with Visualization Subset

--- data_cleaning_subsetting.R
+++ data_cleaning_subsetting.R
@@ -4,18 +4,24 @@ source("facial_zone_dummy.R")
 ## data cleaning, subsetting
 
 ## creating first subset, only contains ICD10 code columns, with our Categorized column J
-mmc_subset_ICD10 <-  mmc_data[c(1:9, 264:267, 48:80, 262)]
+mmc_subset_ICD10 <-  mmc_data[c(1:9, 264:267, 48:80)]
 
 ## removing rows that do not have a value in the ICD10.diagnosis.code.1 variable
 mmc_subset_ICD10 <- mmc_subset_ICD10[complete.cases(mmc_subset_ICD10$ICD10.diagnosis.codes.1),]
 
+mmc_subset_ICD10$sum_dummy <- mmc_subset_ICD10$mandible + mmc_subset_ICD10$superior + mmc_subset_ICD10$midface
+
+mmc_subset_ICD10 <- subset(mmc_subset_ICD10, sum_dummy > 0)
+
+visualization_subset <- mmc_subset_ICD10[c(9:13)]
+
 # ____________________
 
 ## creating another subset which includes both the ICD9 and ICD10 code columns
-mmc_subset_ICD9_ICD10 <- mmc_data[c(1:9, 264:267, 22:80, 262)]
+mmc_subset_ICD9_ICD10 <- mmc_data[c(1:9, 264:267, 22:80)]
 
 ## removing rows that do not have a value in ICD.9.diagnosis.code.1 variable
-mmc_subset_ICD9 <- mmc_data[c(1:9, 264:267, 22:47, 262)]
+mmc_subset_ICD9 <- mmc_data[c(1:9, 264:267, 22:47)]
 mmc_subset_ICD9 <- mmc_subset_ICD9[complete.cases(mmc_subset_ICD9_ICD10$ICD9.diagnosis.codes.1),]
 
 ## Creating test data frame to observe mechanisms vs category
