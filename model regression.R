c4cc287b2545d1e1f7c5191f8d1cd51e7d091875
Mi Zhang <mizh@bu.edu>
2021-12-12 14:28:42 -0500

    glmer

--- model regression.R
+++ model regression.R
@@ -1,10 +1,74 @@
-library(lme4)
+source("cleaning_and_code_conversion.R")
+source("facial_zone_dummy.R")
+source("data_cleaning_subsetting.R")
+pacman::p_load(
+  "ggplot2",
+  "knitr",
+  "arm",
+  "rstanarm",
+  "foreign",
+  "bayesplot",
+  "glmx",
+  "reshape2",
+  "VGAM",
+  "dplyr",
+  "magrittr",
+  "tidyverse",
+  "lme4"
+  )
 
-df <- cleaned_set
+# mandible 
+ml_mandible <- stan_glmer(mandible ~ as.numeric(Age) + Gender  + Injury.Mech.category + 
+                            (1+Urban.Rural|Injury.Mech.category), df, family = binomial, refresh=0)
+summary(mandible)
+plot(ml_mandible)
+pp_check(ml_mandible)
+
+
+# superior
+ml_superior <- stan_glmer(superior ~as.numeric(Age) + Gender  + Injury.Mech.category + 
+                            (1 + Urban.Rural| Injury.Mech.category), data = cleaned_set, 
+                          family=binomial, refresh=0)
+summary(ml_superior)
+plot(ml_superior)
+pp_check(ml_superior)
+
+# midface
+ml_midface <- stan_glmer(midface ~as.numeric(Age) + Gender  + Injury.Mech.category + 
+                            (1 + Urban.Rural| Injury.Mech.category), data = cleaned_set, 
+                          family=binomial, refresh=0)
+summary(ml_midface)
+plot(ml_midface)
+pp_check(ml_midface)
+
+# otherff
+ml_other <- stan_glmer(otherff ~as.numeric(Age) + Gender  + Injury.Mech.category + 
+                           (1 + Urban.Rural| Injury.Mech.category), data = cleaned_set, 
+                         family=binomial, refresh=0)
+summary(ml_other)
+plot(ml_other)
+pp_check(ml_other)
+
+
+
+
+
+
+
+
+## test models below
+
+fit_lm <- lm(midface ~ as.numeric(Age) + Gender + Urban.Rural + Injury.Mech.category, data = cleaned_set)
+par(mfrow=c(2,2))
+plot(fit_lm)
+
 
 ## regression
-fit_mandible <- stan_glm(formula = mandible ~ Age + Gender + Urban.Rural + Injury.Mech.category,  family = binomial(link = "logit"), data = cleaned_set, refresh = 0)
+fit_midface <- stan_glm(midface ~ as.numeric(Age) + Gender + Urban.Rural + Injury.Mech.category,  family = binomial(link = "logit"), data = cleaned_set, refresh = 0)
 round(coef(fit_mandible), digits = 5)
+plot(fit_mandible)
+pp_check(fit_mandible)
+# zero inflation
 
 ## validation
 jitter_binary <- function(a,jitt=0.05){
@@ -18,8 +82,16 @@ ggplot(df)+
   
 predict(fit_mandible, newdata = cleaned_set, type = "response")
 
-## multilevel regression
-ggplot(df) + geom_bar(aes(x=mandible,fill=Urban.Rural))
 
-ml_mandible <- stan_glmer(mandible ~ Age + Gender + Urban.Rural + Injury.Mech.category + 
-                           (1+Urban.Rural|Injury.Mech.category), df, family = binomial, refresh=0)
+
+##
+
+
+
+# glmer_function<- function(para1){
+#   df<- cleaned_set  
+#   facial_trauma<- stan_glmer(para1 ~ as.numeric(Age) + Gender  + Injury.Mech.category + (1+Urban.Rural|Injury.Mech.category), 
+#                              data= df, family = binomial, refresh=0)
+#   return(facial_trauma)
+# }
+
